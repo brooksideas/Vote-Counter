8b2378c50ba8b413cc5f8d84593f4fe1c635f48a
Brook Tarekegn <brooksideas@gmail.com>
2022-04-13 02:53:37 +0300

    Looping issue fixed

--- hash.cpp
+++ hash.cpp
@@ -8,7 +8,7 @@
 #include <sstream>
 
 using namespace std;
-template <typename t1, typename t2>
+template <typename t1, typename t2 = int>
 class hashMap
 {
 public:
@@ -87,7 +87,7 @@ hashMap<t1, t2>::hashMap()
     if (infile.good())
     {
         // Get the number of candidates which is always on the second line
-        numberOfCandidates = stoi(GotoFileLine(infile, 2));  // converts to integer
+        numberOfCandidates = stoi(GotoFileLine(infile, 2)); // converts to integer
         cout << "Candidates" << numberOfCandidates << endl;
     }
 
@@ -102,15 +102,14 @@ hashMap<t1, t2>::hashMap()
     }
 
     // Loop through the candidates list and assign them all as key value pairs in the Hash Map
-    // Candidate / Party value always starts from Line 3 
-    for (int i = 3; i < (numberOfCandidates + i); i+2)
+    // Candidate / Party value always starts from Line 3
+    for (int i = 3; i < (numberOfCandidates + i); i + 2)
     {
-        currentLineNumber = i; // holds the current read line of the file
-        string fileLineKey= GotoFileLine(infile, i); // this is the unique candidate key
-        string fileLineValue= GotoFileLine(infile, i + 1); // this is the unique candidate key
-         
-        hashMap hash;
-        hash[fileLineKey , fileLineValue];    // pass in the returned values to the overloaded operator
+        currentLineNumber = i;                              // holds the current read line of the file
+        string fileLineKey = GotoFileLine(infile, i);       // this is the unique candidate key
+        string fileLineValue = GotoFileLine(infile, i + 1); // this is the unique candidate key
+
+        this->operator[](fileLineKey); // pass in the returned values to the overloaded operator
     }
 }
 
@@ -138,19 +137,9 @@ hashMap<t1, t2>::~hashMap()
 template <typename t1, typename t2>
 t2 &hashMap<t1, t2>::operator[](t1 key)
 {
-   /* Commented temporarily 
-    double loadFactor = items / size;
-    if (loadFactor >= 0.5)
-    {
-        resize();
-    }
-    int x = h(key);
-    if (table[x] == NULL || table[x] == 1 || table[x] == 2)
-    {
-    }
-    */
 
     // Successful so insert into the hashmap
+    cout << "Op KEY ===>" << key << endl;
 
     // Create a new hashpair node
     hashPair *node = new hashPair;
@@ -158,11 +147,30 @@ t2 &hashMap<t1, t2>::operator[](t1 key)
     // To track last node of the list
     hashPair *prev = NULL;
     // Input the Key value pair data
-    
-    node->key = key;
-    node->value = t2(); // default value 
-    node->link = NULL;
 
+    node->key = key;    // Marylin Manson
+    node->value = t2(); // default value = 0
+    node->link = NULL;  // NULL
+    cout << "Node KEY VAL ===>" << node->key << endl;
+    cout << "NV " << node->value << endl;
+    cout << " NL" << node->link << endl;
+
+    // check table X position
+    int x = h(key);
+    cout << "Node X ===>"
+         << " " << x << endl;
+    cout << "Node TABLE X ===>"
+         << " " << table[x] << endl;
+
+    double loadFactor = items / size;
+
+    if (table[x] == NULL || table[x] == 1 || table[x] == 2)
+    {
+        if (loadFactor >= 0.5) // 7 / 5  = 0 , 1/5 = 0.2 , 2/5 = 0.4 , 3/5 = 0.6
+        {
+            resize();
+        }
+    }
     // If the node is first
     if (*(table + 1) == NULL)
     {
@@ -207,7 +215,7 @@ int hashMap<t1, t2>::h(std::string key) const
     }
 
     // mods the ASCII sum by it by the string size and return value
-    value = stringASCIISum % stringSize;
+    value = stringASCIISum % size; // stringSize;
     return value;
 }
 
@@ -271,5 +279,5 @@ int main()
     */
 
     // Create an Instance
-    hashMap<std::string, std::string> myHash;
+    hashMap<std::string, int> myHash;
 }
